6633cee79cb18857ba429988966fc7345b1fdcd2
eleni <126977992+omiridoue@users.noreply.github.com>
2026-09-04 13:41:01 +0100

    Update 1000_model_estimation_selection.R

--- templates/1000_model_estimation_selection.R
+++ templates/1000_model_estimation_selection.R
@@ -34,7 +34,7 @@ print(specification)
 print("====================================")
 
 school <- readRDS("${STR}")
-compChange <- RSiena::as_composition_rsienaFromFile("${compositionFile_period}")
+compChange <- RSiena::sienaCompositionChangeFromFile("${compositionFile_period}")
 
 # Create Behaviour Data Object
 behaviour_matrix <- as.matrix(school[["behaviour"]])
