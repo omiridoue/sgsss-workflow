a785776fa873d041eb6684741c6f476beddc0d76
eleni <126977992+omiridoue@users.noreply.github.com>
2026-09-04 11:22:50 +0100

    Update 1000_model_estimation_no.R
    transforming script to the new names of RSiena version 1.6.

--- templates/1000_model_estimation_no.R
+++ templates/1000_model_estimation_no.R
@@ -4,8 +4,8 @@
 
 # Load Utility Functions
 script_dir <- file.path("${projectDir}", "bin")
-source(file.path(script_dir, "siena07RunToConvergence.R"))
-source(file.path(script_dir, "siena07RunSimOnly.R"))
+source(file.path(script_dir, "sienaRunToConvergence.R"))
+source(file.path(script_dir, "sienaRunSimOnly.R"))
 source(file.path(script_dir, "EgoAlterTable.R"))
 
 # Load Data & Dependencies
@@ -37,7 +37,7 @@ print("====================================")
 
 
 school <- readRDS("${STR}")
-compChange <- RSiena::sienaCompositionChangeFromFile("${compositionFile_period}")
+compChange <- RSiena::as_composition_rsienaFromFile("${compositionFile_period}")
 
 # Create Behaviour Data Object
 behaviour_matrix <- as.matrix(school[["behaviour"]])
@@ -46,24 +46,26 @@ behaviour_matrix[behaviour_matrix %in% c(1)] <- 1
 behaviour_matrix[behaviour_matrix %in% c(2,3,4)] <- 2
 behaviour_matrix[behaviour_matrix %in% c(5,6)] <- 3
 
-smoking <- RSiena::sienaDependent(behaviour_matrix, type = "behavior")
+smoking <- RSiena::as_dependent_rsiena(behaviour_matrix, type = "behavior")
 
 # Constant Covariates over Time
-gender <- RSiena::coCovar(school[["gender"]], warn=FALSE)
+gender <- RSiena::as_covariate_rsiena(school[["gender"]], warn=FALSE)
 
-friends <- sienaDependent(school[["friendship"]])
+friends <- as_dependent_rsiena(school[["friendship"]])
 
 # Load data objects to Siena model
-myData <- RSiena::sienaDataCreate(friends, smoking, gender, compChange)
+myData <- RSiena::make_data_rsiena(friends, smoking, gender, compChange)
 
-myEffects_Network <- RSiena::getEffects(myData)
+myEffects_Network <- RSiena::make_specification(myData)
 
 # Model Specification
-myEffects_Network <- setEffect(myEffects_Network, Rate, name = "friends", type = "rate")
-myEffects_Network <- includeEffects(myEffects_Network, density, recip, gwespFF, name="friends", type = "eval")
-myEffects_Network <- includeEffects(myEffects_Network, linear, name="smoking", type = "eval")
+myEffects_Network <- set_effect(myEffects_Network, Rate, type="rate",
+             depvar="friends")
+myEffects_Network <- set_effect(myEffects_Network, list(density, recip,
+             gwespFF), type="eval", depvar="friends")
+myEffects_Network <- set_effect(myEffects_Network, linear, type="eval",
+             depvar="smoking")
 
-modelOptions <- sienaAlgorithmCreate(MaxDegree=c(friends=6), diagonalize=.2, seed=786840, useStdInits = TRUE, n3 = 100)
 
 effects_info <- as.logical(effects_info)
 cond <- which(effects_info)
@@ -75,71 +77,71 @@ cond <- which(effects_info)
 # }
 
 
- myResults <- RSiena::siena07(modelOptions,
-    data = myData,
-    effects = myEffects_Network,
-    batch=TRUE,
-    verbose=FALSE,
-    silent=TRUE,
-    returnThetas=TRUE,
-    nbrNodes = availableCores,
-    useCluster = TRUE)
+myResults <- RSiena::siena(modelOptions,
+                             data = myData,
+                             effects = myEffects_Network,
+                             batch=TRUE,
+                             verbose=FALSE,
+                             silent=TRUE,
+                             returnThetas=TRUE,
+                             nbrNodes = availableCores,
+                             useCluster = TRUE)
 
 # # ===============================================================================
 
 modelOptions_conv <- RSiena::sienaAlgorithmCreate(
-    MaxDegree = c(friends = 6),
-    diagonalize = .2,
-    seed = 786840,
-    n3 = 10000,
-    firstg = 0.05
+  MaxDegree = c(friends = 6),
+  diagonalize = .2,
+  seed = 786840,
+  n3 = 10000,
+  firstg = 0.05
 ) # the seed is for the lab only
 
 
 modelOptions_sim <- RSiena::sienaAlgorithmCreate(
-    MaxDegree = c(friends = 6),
-    diagonalize = .2,
-    seed = 786840,
-    simOnly = TRUE,
-    nsub = 0,
-    n3 = 500
+  MaxDegree = c(friends = 6),
+  diagonalize = .2,
+  seed = 786840,
+  simOnly = TRUE,
+  nsub = 0,
+  n3 = 500
 ) # the seed is for the lab only
 
 # # # ===============================================================================
-myResults_sim <- siena07RunSimOnly(alg = modelOptions_sim,
-   dat = myData,
-   eff = myEffects_Network,
-   thetaB=Inf,
-   ans0 = myResults,
-   modelName = paste0("${school_period}","_N_"),
-   batch=TRUE,
-   verbose=FALSE,
-   silent=TRUE,
-   returnThetas=TRUE,
-   returnChains=FALSE,
-   returnDeps=TRUE,
-   status = NULL)
+myResults_sim <- sienaRunSimOnly(alg = modelOptions_sim,
+                                   dat = myData,
+                                   eff = myEffects_Network,
+                                   thetaB=Inf,
+                                   ans0 = myResults,
+                                   modelName = paste0("${school_period}","_N_"),
+                                   batch=TRUE,
+                                   verbose=FALSE,
+                                   silent=TRUE,
+                                   returnThetas=TRUE,
+                                   returnChains=FALSE,
+                                   returnDeps=TRUE,
+                                   status = NULL)
 
 
 png(filename=paste0("${school_period}","_N_", "gofIndegrees.png"))
-gofIndegrees <- sienaGOF(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=IndegreeDistribution, cumulative=FALSE, levls=0:6)
+gofIndegrees <- test_gof(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=IndegreeDistribution, cumulative=FALSE, levls=0:6)
 plot(gofIndegrees, main = paste0("${school_period}","_N_", "gofIndegrees"))
 dev.off()
 
 # goodness of fit for outdegree distribution:
 png(filename=paste0("${school_period}","_N_", "gofOutdegrees.png"))
-gofOutdegrees <- sienaGOF(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=OutdegreeDistribution, cumulative=FALSE, levls=0:6)
+gofOutdegrees <- test_gof(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=OutdegreeDistribution, cumulative=FALSE, levls=0:6)
 plot(gofOutdegrees, main = paste0("${school_period}","_N_", "gofOutdegrees"))
 dev.off()
 
 # goodness of fit for triad census:
 png(filename=paste0("${school_period}","_N_", "gofTriads.png"))
-gofTriads <- sienaGOF(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=TriadCensus, verbose=TRUE,join=TRUE)
+gofTriads <- test_gof(sienaFitObject=myResults_sim, varName="friends", auxiliaryFunction=TriadCensus, verbose=TRUE,join=TRUE)
 plot(gofTriads, main = paste0("${school_period}","_","_N_", "gofTriads"))
 dev.off()
 
 png(filename=paste0("${school_period}","_N_", "gofEgoAlterTable.png"))
-gof.EgoAlterTable <- sienaGOF(myResults_sim,EgoAlterTable,
+gof.EgoAlterTable <- test_gof(myResults_sim,EgoAlterTable,
                               verbose=TRUE,join=TRUE,varName=c("friends","smoking"))
 plot(gof.EgoAlterTable, main = paste0("${school_period}","_N_", "gofEgoAlterTable"))
 dev.off()
