a577f6e636c6ae6b75257f1ef8eb4efb4f13f1e7
eleni <126977992+omiridoue@users.noreply.github.com>
2026-09-04 23:25:50 +0100

    Update sienaRunSimOnly.R

--- bin/sienaRunSimOnly.R
+++ bin/sienaRunSimOnly.R
@@ -1,18 +1,18 @@
 #!/usr/bin/env Rscript
 
 sienaRunSimOnly <- function(alg, dat, eff, thetaB, ans0, modelName, batch, verbose, silent, returnThetas, returnChains, returnDeps, status, ...)  {
-  numr <- 0 
+  numr <- 0
   ans <- ans0
 
-  repeat { 
-    numr <- numr+1 # count number of repeated runs 
-    tm <- ans$tconv.max # convergence indicator 
-    
+  repeat {
+    numr <- numr+1 # count number of repeated runs
+    tm <- ans$tconv.max # convergence indicator
+
     alg$nsub <- 0
     alg$simOnly <- TRUE
 
     alg$n3 <- 1000
-    
+
     if (isTRUE(tm < 0.25)) {
       status <- "_CONVERGED"
       break
@@ -21,9 +21,9 @@ sienaRunSimOnly <- function(alg, dat, eff, thetaB, ans0, modelName, batch, verbo
       status <- "_NOTCONVERGED"
       break
       }
-    ans <- siena(control_algo = alg, data=dat, effects=eff, prevAns=ans, thetaBound = thetaB, batch=batch, verbose=verbose, silent=silent, returnThetas=returnThetas, returnChains=returnChains, returnDeps=returnDeps)
-  
-  } 
+    ans <- siena07(alg, data=dat, effects=eff, prevAns=ans, thetaBound = thetaB, batch=batch, verbose=verbose, silent=silent, returnThetas=returnThetas, returnChains=returnChains, returnDeps=returnDeps)
+
+  }
   saveRDS(ans, file = paste0(modelName, "ITER", numr, status,"_SIM.RDS"))
-  return(ans) 
+  return(ans)
 }
