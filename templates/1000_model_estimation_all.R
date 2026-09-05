1e4e01e273737447bb12f9f72c52aff20ffa6747
eleni <126977992+omiridoue@users.noreply.github.com>
2026-09-05 22:01:42 +0100

    Update 1000_model_estimation_all.R

--- templates/1000_model_estimation_all.R
+++ templates/1000_model_estimation_all.R
@@ -57,7 +57,6 @@ myEffects_Network <- RSiena::make_specification(myData)
 # Model Specification
 myEffects_Network <- set_effect(myEffects_Network, Rate, type="rate",
              depvar="friends")
-myEffects_Network <- set_effect(myEffects_Network, transTrip1, verbose = FALSE)
 
 myEffects_Network <- set_effect(myEffects_Network, linear, type="eval",
              depvar="smoking")
